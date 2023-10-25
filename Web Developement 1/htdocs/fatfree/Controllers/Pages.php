a4dedbd4c990bfd8aa5304990b25608979f40cdf
Stephanie Moreau <stephanie.moreau@johnabbott.qc.ca>
2023-10-25 11:00:29 -0400

    F3 - Controllers

---
+++ Web Developement 1/htdocs/fatfree/Controllers/Pages.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Handle pages without database connections
+ */
+class Pages{
+
+	function homepage($f3){
+		$aboutPageURL = $f3->alias("aboutPage");
+
+		echo "This is the homepage
+		<br>
+		<a href='aboutUs'>My about page</a>
+		<br>
+		<a href='{$aboutPageURL}'>My about page</a>
+		";
+	}
+
+	function about(){
+		echo "About Me.. Hello";
+	}
+
+	function aboutPost(){
+		echo "Post method used";
+	}
+
+	function userData($f3, $params){
+		echo "User id: " . $f3->get("PARAMS.uid") . "<br>";
+		echo "User id: " . $params['uid'];
+	}
+
+
+}
--- Web Developement 1/htdocs/fatfree/index.php
+++ Web Developement 1/htdocs/fatfree/index.php
@@ -1,4 +1,5 @@
 <?php
+//TODO:  fix alias prefix
 
 // will automatically load any library required from our composer
 require "vendor/autoload.php";
@@ -6,31 +7,41 @@ require "vendor/autoload.php";
 // load framework
 $f3 = Base::instance();
 
-// setup simple route
-$f3->route('GET /', function($f3){ 
-	//$f3 as argument allows access to global $f3
-	echo "Hello World!";
-});
+// framework to automatically load the classees within these folders
+$f3->set('AUTOLOAD', 'Controllers/');
 
-$f3->route('GET /about', function($f3){
-	echo "About Me";
-});
+/* - used without controller folder
+	// setup simple route
+	$f3->route('GET /', function($f3){ 
+		//$f3 as argument allows access to global $f3
+		echo "Hello World!";
+	});
 
-$f3->route('POST /about', function($f3){
-	echo "About the post method";
-});
+	$f3->route('GET /about', function($f3){
+		echo "About Me";
+	});
 
+	$f3->route('POST /about', function($f3){
+		echo "About the post method";
+	});
 
-$f3->route('GET /single/@singleid', function($f3, $p){
-	echo "Single ". $f3->get('PARAMS.singleid') .'<br>';
-	echo "parameter " . $p['singleid'] . '<br>';
-});
+	$f3->route('GET /single/@singleid', function($f3, $p){
+		echo "Single ". $f3->get('PARAMS.singleid') .'<br>';
+		echo "parameter " . $p['singleid'] . '<br>';
+	});
 
-$f3->route('GET /single/@singleid/@category', function($f3, $p){
-	echo "Single ". $f3->get('PARAMS.singleid') .'<br>';
-	echo "parameter " . $p['singleid'] . '<br>';
-	echo $p['category'];
-});
+	$f3->route('GET /single/@singleid/@category', function($f3, $p){
+		echo "Single ". $f3->get('PARAMS.singleid') .'<br>';
+		echo "parameter " . $p['singleid'] . '<br>';
+		echo $p['category'];
+	});
+*/
+// routes using /Controller classes
+$f3->route('GET /', 'Pages->homepage');
+$f3->route('GET @aboutPage: /aboutSomebody', 'Pages->about');
+$f3->route('POST /about', 'Pages->aboutPost');
+
+$f3->route('GET /users/@uid', 'Pages->userData'); // not a great example
 
 
 // start the framework
